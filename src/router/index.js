702105c8eed92b43d1185e7473203d1c2bec7596
engquistingrid-lab <engquistingrid@gmail.com>
2025-12-05 15:18:07 +0100

    starta ny grupp sidan

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import StartView from '../views/StartView.vue'
+import StartView from '../views/AdministratorStart.vue'
+import AdministratorStart from '../views/AdministratorStart.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'Start',
-      component: StartView
+      name: 'AdministratorStart',
+      component: AdministratorStart
     },
       {
       path: '/poll/:id',
